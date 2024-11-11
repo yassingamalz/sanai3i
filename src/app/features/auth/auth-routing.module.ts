f549680a561e49ab4d9d9511fd5917fd79324e6a
yassingamalz <yassen.gamal.yg@gmail.com>
2024-11-11 12:30:59 +0000

    fix(routing): Configure auth module routing - Default redirect to welcome

--- src/app/features/auth/auth-routing.module.ts
+++ src/app/features/auth/auth-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WelcomeComponent } from './pages/welcome/welcome.component';
+import { UserTypeComponent } from './pages/user-type/user-type.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'welcome',
+    component: WelcomeComponent
+  },
+  {
+    path: 'user-type',
+    component: UserTypeComponent
+  },
+  {
+    path: '',
+    redirectTo: 'welcome',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
