a7e2840b848977c88b5866b26390b923ed3b737b
yassingamalz <yassen.gamal.yg@gmail.com>
2024-11-14 15:23:36 +0000

    fix: remove animation delay from welcome component

--- src/app/features/auth/pages/welcome/welcome.component.ts
+++ src/app/features/auth/pages/welcome/welcome.component.ts
@@ -1,28 +1,25 @@
 
-  
-  // welcome.component.ts
-  import { Component, AfterViewInit } from '@angular/core';
-  import { Router } from '@angular/router';
-  
-  @Component({
-    selector: 'app-welcome',
-    templateUrl: './welcome.component.html',
-    styleUrls: ['./welcome.component.scss']
-  })
-  export class WelcomeComponent implements AfterViewInit {
-    constructor(private router: Router) {}
-  
-    ngAfterViewInit() {
-      // Add animation class after a delay
-      window.setTimeout(() => {
-        const svg = document.querySelector('svg');
-        if (svg) {
-          svg.classList.add('animated');
-        }
-      }, 1000);
-    }
-  
-    navigate(route: string): void {
-      this.router.navigate(['/auth/'+route]);
+
+// welcome.component.ts
+import { Component, AfterViewInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-welcome',
+  templateUrl: './welcome.component.html',
+  styleUrls: ['./welcome.component.scss']
+})
+export class WelcomeComponent implements AfterViewInit {
+  constructor(private router: Router) { }
+
+  ngAfterViewInit() {
+    const svg = document.querySelector('svg');
+    if (svg) {
+      svg.classList.add('animated');
     }
   }
+
+  navigate(route: string): void {
+    this.router.navigate(['/auth/' + route]);
+  }
+}
